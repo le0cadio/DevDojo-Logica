5ef1bc6d88648d0d56ca18119e9b91223a7f104d
gleocadi0 <guilhermeleocadio1301@gmail.com>
2023-07-18 13:16:31 -0300

    Add the first exercise, calculator between ages

---
+++ src/CalculatorbetweenAges.java
@@ -0,0 +1,9 @@
+public class CalculatorbetweenAges {
+    public static void main(String[] args) {
+        int firstAge = 10;
+        int secondAge = 20;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("The difference between ages is: " + ageDifference);
+
+    }
+}
