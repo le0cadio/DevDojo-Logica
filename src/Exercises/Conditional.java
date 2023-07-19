72274a8fefb7845639be5a0650b9ddb6f0a82214
gleocadi0 <guilhermeleocadio1301@gmail.com>
2023-07-19 11:29:07 -0300

    flux control if else

---
+++ src/Exercises/Conditional.java
@@ -0,0 +1,11 @@
+package Exercises;
+
+public class Conditional {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("This is true");
+        } else {
+            System.out.println("This is false");
+        }
+    }
+}
