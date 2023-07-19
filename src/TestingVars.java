1a8635ebcaf4315b6a581787a7c9dada231aa805
gleocadi0 <guilhermeleocadio1301@gmail.com>
2023-07-19 11:02:25 -0300

    testing vars

---
+++ src/TestingVars.java
@@ -0,0 +1,22 @@
+public class TestingVars {
+    public static void main(String[] args) {
+        char letter = 'a';
+        System.out.println(letter);
+
+        int number = 10;
+        System.out.println(number);
+
+        float decimal = 10.5F;
+        System.out.println(decimal);
+
+        double decimal2 = 10.5;
+        System.out.println(decimal2);
+
+        boolean flag = true;
+        System.out.println(flag);
+
+        String name = "Leo";
+        System.out.println(name);
+
+    }
+}
