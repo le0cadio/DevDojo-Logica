6dba6c4ff1e08f8c9d15a0487c67049ffc0bcf45
gleocadi0 <guilhermeleocadio1301@gmail.com>
2023-07-24 17:08:15 -0300

    Exercise 5 - Odd or even

---
+++ src/Exercises/Exercise5.java
@@ -0,0 +1,14 @@
+//based on a number, print if is odd or even
+
+package Exercises;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number % 2 == 0) {
+            System.out.println("Even!");
+        } else {
+            System.out.println("Odd!");
+        }
+    }
+}
