a173061b5b3c994281a9c4f693c0b25b09fc5d2e
gleocadi0 <guilhermeleocadio1301@gmail.com>
2023-08-31 18:31:06 -0300

    wip

---
+++ src/UrnaEletronica.java
@@ -0,0 +1,15 @@
+public class UrnaEletronica {
+   //desenvolve um algoritmo de urna eletronica
+
+    public static void main(String[] args) {
+        int age = 22;
+        if (age >= 18) {
+            System.out.println("Adult!");
+
+        } else {
+            System.out.println("Non-adult!");
+        }
+    }
+
+
+}
